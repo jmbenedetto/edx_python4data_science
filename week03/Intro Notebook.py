30680fcd330d7580171cec54f4033132bbf5b362
jmbenedetto <jmbenedetto@gmail.com>
2021-10-02 19:10:43 -0300

    week 3 part 1

---
+++ week03/Intro Notebook.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:hydrogen
+#     text_representation:
+#       extension: .py
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.12.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% pycharm={"name": "#%%\n"}
+print("Hello World!")
+
+# %% [markdown] pycharm={"name": "#%% md\n"} active="ipynb"
+# Below is how we calculate the number of seconds in a year
+
+# %% pycharm={"name": "#%%\n"} active="ipynb"
+# 365*24*60*60
+
+# %% pycharm={"name": "#%%\n"}
+_ /1e6
+
+# %% pycharm={"name": "#%%\n"}
+366*24*60*60
+
+# %% pycharm={"name": "#%%\n"}
+_ /1e6
+
+# %% pycharm={"name": "#%%\n"}
+x = 4 + 3
+print(x)
+
+# %% [markdown] pycharm={"name": "#%% md\n"}
+# # This is a markdown cell
+# ## this is heading 2
+# ### this is heading 3
+# Hi!
+#
+# * one fish
+# * two fish
+#
+# **bold**
+# *italic*
+
+# %% [markdown] pycharm={"name": "#%% md\n"}
+# $\int_Oˆ\infty x^{-\alpha}$
+
+# %% pycharm={"name": "#%%\n"}
+%matplotlib inline
+from matplotlib.pyplot import plot
+
+plot([0,1,0,1])
+
+# %% [markdown] pycharm={"name": "#%% md\n"}
+# This is a plot using matplotlib.
